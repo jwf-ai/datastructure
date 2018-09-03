7874e89be8358e4fc1af6d47136059890b5f3edf
jwf-ai <jianwenfu@foxmail.com>
2018-09-03 16:40:17 +0800

    update

--- Sort/InsertSort.py
+++ Sort/InsertSort.py
@@ -1,17 +1,15 @@
 # encoding: utf-8
 
-arr = [2,4,5,3,4,8,6,4,5,7,3,8]
+arr = [2,5,4,3,7,1]
 
 def insert_sort(arr):
     for i in range(1,len(arr)):
-        if arr[i] < arr[i-1]:
-            temp = arr[i]
-            for j in range(i-1,0,-1):
-                if temp < arr[j]:
-                    arr[j+1] = arr[j]
-                else:
-                    arr[j] = temp
-                    break
+        temp = arr[i]
+        j = i
+        while j > 0 and arr[j-1] > temp:
+            arr[j] = arr[j-1]
+            j -= 1
+        arr[j] = temp
     return arr
 
 print(insert_sort(arr))
