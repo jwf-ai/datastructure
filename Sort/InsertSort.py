d113ee7771e68bbb31bd852ec65e86cfa6aea50f
jwf-ai <jianwenfu@foxmail.com>
2018-09-03 16:53:53 +0800

    update

--- Sort/InsertSort.py
+++ Sort/InsertSort.py
@@ -2,6 +2,7 @@
 
 arr = [2,5,4,3,7,1]
 
+# 直接插入
 def insert_sort(arr):
     for i in range(1,len(arr)):
         temp = arr[i]
@@ -12,4 +13,25 @@ def insert_sort(arr):
         arr[j] = temp
     return arr
 
+# 二分插入
+def insert_sort_binary(arr):
+    for i in range(1,len(arr)):
+        if arr[i] < arr[i-1]:
+            left = 0
+            right = i - 1
+            temp = arr[i]
+
+            while left <= right:
+                mid = (left + right)//2
+                if arr[i] < arr[mid]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+
+            for j in range(i-1,left-1,-1):
+                arr[j+1] = arr[j]
+            arr[left] = temp
+    return arr
+
 print(insert_sort(arr))
+print(insert_sort_binary(arr))
